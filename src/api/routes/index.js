9a2bc42a73d0b4835b0a33cbb5a45d50df6c460b
Pakala Sai Charan Kumar <103536125+Saicharan-chc@users.noreply.github.com>
2022-05-04 22:27:52 +0530

    add health (#2)
    Co-authored-by: Rathindranath Dutta <rathindranath.dutta@crystaltechlab.com>

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express'),
     router = express.Router();
 
+router.use('/health', (req,res)=>{
+    res.send('OK');
+});
 router.use('/auth', require('./auth/auth'));
 
 module.exports = router;
